f597ff09214b9aee41aad68e69e72e78b099266f
basakk123 <basakk@naver.com>
2022-11-15 17:19:49 +0900

    뷰모델 예제

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:data_app/views/product/list/product_list_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(
+    // 위젯에서 프로바이더를 사용하고 읽기위해
+    // 앱 전체적으로 "ProviderScope" 위젯을 감싸줘야 합니다.
+    // 여기에 프로바이더의 상태가 저장됩니다.
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: ProductListPage(),
+    );
+  }
+}
